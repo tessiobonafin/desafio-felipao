3fd46103c9e806950f5899e376d1ac7014c29d0c
tessiobonafin <tebonafin@gmail.com>
2023-12-13 16:23:10 -0300

    projeto dio atualização

---
+++ index.js
@@ -0,0 +1,34 @@
+//Se XP for menor do que 1.000 = Ferro
+let level = ["Ferro" , "Bronze" , "Prata" , "Ouro" , "Platina" , "Ascendente" , "Imortal" , "Radiante"]
+let pontuacao = ["menor do que 1.000" , "entre 1.001 e 2.000" , "entre 2.001 e 5.001" , "entre 6.001 e 7.000" , "entre 7.001 e 8.000" , "entre 8.001 e 9.000" , "entre 9.001 e 10.000" , "maior ou igaul a 10.001"]
+console.log("Se XP for " + pontuacao[0] + " = " + level[0])
+
+//"O Herói de nome **{nome}** está no nível de **{nivel}**"
+
+let nickname = "Super Dev Beginer " 
+console.log ("O Herói de nome " + nickname + "está no nível de experiência " + level[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
